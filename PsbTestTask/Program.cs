5ff03d4b1a1d2b1dc27349d2ee5d12000d0f760c
oqo0 <oq.roman@gmail.com>
2024-03-13 18:28:45 +0300

    Refactor client code

--- PsbTestTask/Program.cs
+++ PsbTestTask/Program.cs
@@ -6,19 +6,33 @@ public static class Program
 {
     public static void Main()
     {
+        var games = GetGamesFromUserInput();
+
+        DisplayGameResults(games);
+    }
+
+    private static List<Game?> GetGamesFromUserInput()
+    {
         Console.WriteLine("Введите список матчей (<команда 1>:<команда 2>, <команда 1>:<команда 2> ...)");
         var gamesInput = Console.ReadLine();
+
         if (string.IsNullOrEmpty(gamesInput))
-            return;
+        {
+            Console.WriteLine("No games input provided.");
+            return null;
+        }
 
         var gamesInputData = gamesInput.Split(", ");
-        var games = gamesInputData
-            .Select(Game.Parse)
-            .ToList();
+        var games = gamesInputData.Select(Game.Parse).ToList();
 
+        return games;
+    }
+
+    private static void DisplayGameResults(List<Game?> games)
+    {
         var gameManager = new GamesManager();
         var gameTable = gameManager.GetGameTable(games);
 
-        Console.WriteLine($"Команда №1 - {gameTable[0]} очка, Команда №2 - {gameTable[1]} очков.");
+        Console.WriteLine($"Команда №1 - {gameTable[0]}, Команда №2 - {gameTable[1]}");
     }
 }
